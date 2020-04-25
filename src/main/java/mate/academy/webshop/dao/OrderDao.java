69c0f7660e716261c3e6443e47dfe34762881eba
olehedza <olehedza@gmail.com>
2020-04-25 20:58:01 +0300

    add configurations for maven and do refactor

--- src/main/java/mate/academy/webshop/dao/OrderDao.java
+++ src/main/java/mate/academy/webshop/dao/OrderDao.java
@@ -1,18 +1,6 @@
 package mate.academy.webshop.dao;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.webshop.model.Order;
 
-public interface OrderDao {
-
-    Order create(Order order);
-
-    Optional<Order> get(Long id);
-
-    List<Order> getAll();
-
-    Order update(Order order);
-
-    boolean delete(Long id);
+public interface OrderDao extends GenericDao<Order> {
 }
