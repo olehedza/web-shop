9dbb248a5449ae40d03480c9ba3187912e6917fa
olehedza <olehedza@gmail.com>
2020-05-07 10:49:55 +0300

    delete redundant if

--- src/main/java/mate/academy/webshop/controllers/LogoutController.java
+++ src/main/java/mate/academy/webshop/controllers/LogoutController.java
@@ -7,15 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 public class LogoutController extends HttpServlet {
-    private static final Logger logger = Logger.getLogger(LogoutController.class);
+    private static final Logger LOGGER = Logger.getLogger(LogoutController.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        if (logger.isInfoEnabled()) {
-            logger.info(String.format("User with id %s logout",
-                    req.getSession().getAttribute("user_id")));
-        }
+        LOGGER.info(String.format("User with id %s logout",
+                req.getSession().getAttribute("user_id")));
+
         req.getSession().invalidate();
         resp.sendRedirect(req.getContextPath() + "/");
     }
