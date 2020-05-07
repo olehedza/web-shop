bf1e9fe0d39cadc288c5745714497d4162f24814
Oleksii <olehedza@gmail.com>
2020-05-07 11:58:51 +0300

    Hw-17 (#9)
    * add logs/* to .gitignore
    
    * add log.properties file, add return statements and logger
    
    * add new empty line
    
    * remove redundant if statement
    
    * change logger to LOGGER
    
    * delete redundant if
    
    * delete redundant return statements

--- src/main/java/mate/academy/webshop/controllers/LoginController.java
+++ src/main/java/mate/academy/webshop/controllers/LoginController.java
@@ -10,8 +10,11 @@ import mate.academy.webshop.exceptions.AuthenticationException;
 import mate.academy.webshop.lib.Injector;
 import mate.academy.webshop.model.User;
 import mate.academy.webshop.security.AuthenticationService;
+import org.apache.log4j.Logger;
 
 public class LoginController extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(LoginController.class);
+
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.webshop");
     private final AuthenticationService authService = (AuthenticationService) INJECTOR
             .getInstance(AuthenticationService.class);
@@ -33,7 +36,9 @@ public class LoginController extends HttpServlet {
             User user = authService.login(login, pwd);
             HttpSession session = req.getSession();
             session.setAttribute("user_id", user.getId());
+            LOGGER.info(String.format("Create session for user id:%d", user.getId()));
         } catch (AuthenticationException e) {
+            LOGGER.error("Authentication error: " + e.getMessage(), e);
             req.setAttribute("errorMsg", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp")
                     .forward(req, resp);
--- src/main/java/mate/academy/webshop/controllers/LogoutController.java
+++ src/main/java/mate/academy/webshop/controllers/LogoutController.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 
 public class LogoutController extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(LogoutController.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
+        LOGGER.info(String.format("User with id %s logout",
+                req.getSession().getAttribute("user_id")));
+
         req.getSession().invalidate();
         resp.sendRedirect(req.getContextPath() + "/");
     }
