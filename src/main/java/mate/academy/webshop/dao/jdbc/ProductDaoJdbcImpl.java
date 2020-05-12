7fe336dff8c2d7d858c5e2b724a88071e5933808
olehedza <olehedza@gmail.com>
2020-05-12 13:35:46 +0300

    do refactor in dao jdbc layer

--- src/main/java/mate/academy/webshop/dao/jdbc/ProductDaoJdbcImpl.java
+++ src/main/java/mate/academy/webshop/dao/jdbc/ProductDaoJdbcImpl.java
@@ -20,51 +20,46 @@ public class ProductDaoJdbcImpl implements ProductDao {
     @Override
     public Product create(Product product) {
         String query = "INSERT INTO products (name, price) VALUES (?, ?)";
-        Product createdProduct = null;
 
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, product.getName());
             statement.setDouble(2, product.getPrice());
             statement.executeUpdate();
-            List<Product> list = getAll();
-            createdProduct = list.get(list.size() - 1);
+            return getAll().stream()
+                    .reduce((first, second) -> second)
+                    .orElseThrow(SQLException::new);
         } catch (SQLException e) {
             LOGGER.error("Can't create product", e);
-            return null;
+            throw new RuntimeException("Can't create product");
         }
-        return createdProduct;
     }
 
     @Override
     public Optional<Product> get(Long id) {
         String query = "SELECT * FROM products WHERE id = ?";
-        Product product;
 
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setLong(1, id);
-            product = getProductFromResultSet(statement.executeQuery());
+            return getProductFromResultSet(statement.executeQuery());
         } catch (SQLException e) {
             LOGGER.error(String.format("Can't get product with id: %d!", id), e);
             return Optional.empty();
         }
-        return Optional.of(product);
     }
 
     @Override
     public List<Product> getAll() {
         String query = "SELECT * FROM products";
-        List<Product> products;
 
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(query);
-            products = getAllProductsFromResultSet(statement.executeQuery());
+            return getAllProductsFromResultSet(statement.executeQuery());
         } catch (SQLException e) {
             LOGGER.error("Can't get list of products!", e);
             return new ArrayList<>();
         }
-        return products;
     }
 
     @Override
@@ -77,12 +72,12 @@ public class ProductDaoJdbcImpl implements ProductDao {
             statement.setDouble(2, product.getPrice());
             statement.setLong(3, product.getId());
             statement.executeUpdate();
+            return product;
         } catch (SQLException e) {
             LOGGER.error(String.format("Can't update product with id: %d!",
                     product.getId()), e);
-            return null;
+            throw new RuntimeException("Can't update product");
         }
-        return product;
     }
 
     @Override
@@ -100,16 +95,12 @@ public class ProductDaoJdbcImpl implements ProductDao {
         return true;
     }
 
-    public Product getProductFromResultSet(ResultSet resultSet)
+    public Optional<Product> getProductFromResultSet(ResultSet resultSet)
             throws SQLException {
-        Product product;
-
-        long productId = resultSet.getLong("id");
+        Long productId = resultSet.getLong("id");
         String name = resultSet.getString("name");
-        double price = resultSet.getDouble("price");
-        product = new Product(name, price);
-        product.setId(productId);
-        return product;
+        Double price = resultSet.getDouble("price");
+        return Optional.of(new Product(productId, name, price));
     }
 
     public List<Product> getAllProductsFromResultSet(ResultSet resultSet)
