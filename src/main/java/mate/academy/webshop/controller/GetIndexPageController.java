5ad56681069fbb1f30b294f63bc6552c93212bfb
olehedza <olehedza@gmail.com>
2020-05-28 15:58:58 +0300

    rename controller

--- src/main/java/mate/academy/webshop/controller/GetIndexPageController.java
+++ src/main/java/mate/academy/webshop/controller/GetIndexPageController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class IndexController extends HttpServlet {
+public class GetIndexPageController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
